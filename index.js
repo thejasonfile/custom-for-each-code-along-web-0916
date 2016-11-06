ac534b797ea2316eb71b80486bde452effe34822
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-05 21:03:07 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function forEach(iterable, callback){
+  if (Array.isArray(iterable)){
+    for (let i = 0; i < iterable.length; i++){
+      var element = iterable[i];
+      callback(element, i, iterable);
+    }
+  } else if (typeof iterable == 'object'){
+    for (var key in iterable){
+      var value = iterable[key];
+      callback(key, value, iterable);
+    }
+  }
+}
